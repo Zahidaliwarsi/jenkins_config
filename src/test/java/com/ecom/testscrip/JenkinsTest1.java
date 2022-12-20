303ecfe2f5cfb51e1bd761cbca2b1b50bfcf24f9
qqqqqqqqqqqqqqqqqqqq <qqqqqqqqqqqqqqqqqqqq@DESKTOP-Q88ETHJ>
2022-12-20 08:14:11 -0800

    committing first project

---
+++ src/test/java/com/ecom/testscrip/JenkinsTest1.java
@@ -0,0 +1,12 @@
+package com.ecom.testscrip;
+
+import org.testng.annotations.Test;
+
+public class JenkinsTest1 {
+	@Test(groups="sanity")
+	public void jenkinstest1()
+	{
+		System.out.println("printed");
+	}
+
+}
